7618715136d824318a457d8edade5fb765f2ae2c
Carlos Antonio Freitas <45669251+CarlosAntonioFreitasSilva@users.noreply.github.com>
2021-08-03 19:32:09 -0300

    Add files via upload

---
+++ ajax/request2/request.php
@@ -0,0 +1,8 @@
+<?php 
+	
+	
+	$valorInput = $_GET['item']; //variavel recebe o valor do parametro item enviado via http.
+
+	echo "<p> O valor do input marcado é ".$valorInput;
+
+ ?>
---
+++ ajax/request3/request.php
@@ -0,0 +1,8 @@
+<?php 
+	
+	
+	$nomeInput = $_GET['nome']; //variavel recebe o valor do parametro nome enviado pela url.
+
+	echo "<p> O nome enviado é ".$nomeInput."</p>";
+
+ ?>
